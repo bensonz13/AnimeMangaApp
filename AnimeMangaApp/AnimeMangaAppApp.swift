d109cd2ecc5fa69540b153f3f8a6b78eb64eba48
BT_C09_21 <mobileappdev@bths.edu>
2026-04-24 13:43:11 -0400

    Initial Commit

---
+++ AnimeMangaApp/AnimeMangaAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  AnimeMangaAppApp.swift
+//  AnimeMangaApp
+//
+//  Created by Student on 4/24/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AnimeMangaAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
