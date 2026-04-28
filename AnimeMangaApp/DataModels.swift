659dd59a4eada0943ccdfd6edc57ab1c0a555e90
BT_C09_21 <mobileappdev@bths.edu>
2026-04-28 13:04:56 -0400

    Add NetworkClient and DataModels to make requests.

--- AnimeMangaApp/DataModels.swift
+++ AnimeMangaApp/DataModels.swift
@@ -1,26 +1,43 @@
 struct AnimeResponse: Codable {
-    var current_page: Int
-    var data: [Anime]
+    let data: [Anime]
+    let pagination: Pagination
+}
+
+struct AnimeDetailResponse: Codable {
+    let data: Anime
 }
 
 struct Anime: Identifiable, Codable {
+    let mal_id: Int
     var id: Int { mal_id }
-    
-    var mal_id: Int
-    var url: String?
-    var images: AnimeImages
-    var title_english: String
-    var title_japanese: String
-    var episodes: Int
-    var status: String
+    let title: String
+    let images: Images?
 }
 
-struct AnimeImages: Codable {
-    let jpg: ImageDetails
+struct MangaResponse: Codable {
+    let data: [Manga]
+    let pagination: Pagination
 }
 
-struct ImageDetails: Codable {
+struct MangaDetailResponse: Codable {
+    let data: Manga
+}
+
+struct Manga: Identifiable, Codable {
+    let mal_id: Int
+    var id: Int { mal_id }
+    let title: String
+    let images: Images?
+}
+
+struct Images: Codable {
+    let jpg: JPG
+}
+
+struct JPG: Codable {
     let image_url: String
-    let small_image_url: String
-    let large_image_url: String
+}
+
+struct Pagination: Codable {
+    let has_next_page: Bool
 }
--- AnimeMangaApp/NetworkClient.swift
+++ AnimeMangaApp/NetworkClient.swift
@@ -4,16 +4,110 @@
 //
 //  Created by Student on 4/24/26.
 //
-
 import SwiftUI
 
-struct NetworkClient: View {
-    var baseURL = "https://api.jikan.moe/v4/"
-    var body: some View {
-        Text("Hello, World123!")
+@Observable
+class NetworkClient {
+    private let baseURL = "https://api.jikan.moe/v4"
+    
+    private var topAnime: [Anime] = []
+    private var topManga: [Manga] = []
+    
+    private var animePage: Int = 1
+    private var mangaPage: Int = 1
+    
+    private(set) var selectedAnime: Anime? = nil
+    private(set) var selectedManga: Manga? = nil
+    
+    func getTopAnime() async {
+        let urlStr = "\(baseURL)/top/anime?page=\(animePage)"
+        guard let url = URL(string: urlStr) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(AnimeResponse.self, from: data)
+            
+            for item in response.data {
+                if !topAnime.contains(where: { $0.mal_id == item.mal_id }) {
+                    topAnime.append(item)
+                }
+            }
+            
+            animePage += 1
+        } catch {
+            print(error)
+        }
     }
-}
-
-#Preview {
-    NetworkClient()
+    
+    func getTopManga() async {
+        let urlStr = "\(baseURL)/top/manga?page=\(mangaPage)"
+        guard let url = URL(string: urlStr) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(MangaResponse.self, from: data)
+            
+            for item in response.data {
+                if !topManga.contains(where: { $0.mal_id == item.mal_id }) {
+                    topManga.append(item)
+                }
+            }
+            
+            mangaPage += 1
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getAnimeByID(id: Int) async {
+        let urlStr = "\(baseURL)/anime/\(id)"
+        guard let url = URL(string: urlStr) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(AnimeDetailResponse.self, from: data)
+            selectedAnime = response.data
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getMangaByID(id: Int) async {
+        let urlStr = "\(baseURL)/manga/\(id)"
+        guard let url = URL(string: urlStr) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(MangaDetailResponse.self, from: data)
+            selectedManga = response.data
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getRandomAnime() async {
+        let urlStr = "\(baseURL)/random/anime"
+        guard let url = URL(string: urlStr) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(AnimeDetailResponse.self, from: data)
+            selectedAnime = response.data
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getRandomManga() async {
+        let urlStr = "\(baseURL)/random/manga"
+        guard let url = URL(string: urlStr) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(MangaDetailResponse.self, from: data)
+            selectedManga = response.data
+        } catch {
+            print(error)
+        }
+    }
 }
